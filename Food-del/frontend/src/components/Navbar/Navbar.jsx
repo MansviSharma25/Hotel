1dab972cffc83a5cd0a206490dd2aaedce8df88e
MansviSharma25 <mansvisharma1251@gmail.com>
2024-11-02 23:59:15 +0530

    a

---
+++ Food-del/frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src="assets.logo" alt="" className="logo" />
+<ul className="navbar-menu">
+  <li>Home</li>
+  <li>Menu</li>
+  <li>Mobile App</li>
+  <li>Contact Us</li>
+</ul>
+
+<div className="navbar-right">
+  <img src="assets.search_icon" alt="" />
+</div>
+    </div>
+  )
+}
+
+export default Navbar
